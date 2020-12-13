d21ab037324391d6f17198cfe5d8ade0b3220e5a
redrockstyle <ulowpie@gmail.com>
2020-12-13 20:07:40 +0500

    [Khazov] Task02

--- task02/src/com/example/task02/Task02Main.java
+++ task02/src/com/example/task02/Task02Main.java
@@ -1,23 +1,33 @@
 package com.example.task02;
 
+import java.util.function.IntSupplier;
+import java.util.function.Predicate;
 import java.util.stream.IntStream;
 
 public class Task02Main {
 
     public static void main(String[] args) {
 
-        /*
         cycleGrayCode(2)
                 .limit(10)
                 .forEach(System.out::println);
-        */
+
 
     }
 
     public static IntStream cycleGrayCode(int n) {
-
-        return null; // your implementation here
-
+        if(n>16 || n<1){
+            throw new IllegalArgumentException("not number between [1,16]");
+        }
+        int mod = (int)Math.pow(2,n);
+        return IntStream.generate(new IntSupplier() {
+            int num = 0;
+            @Override
+            public int getAsInt() {
+                if (num == mod) num = 0;
+                return (num ^ (num++>>1)) & mod-1;
+            }
+        });
     }
 
 }
