ff0c09f61c3c8b9747c6f6d323a9f9cf7fc23622
Виталий Вересников <Vitosiy21@yandex.ru>
2020-12-14 21:12:55 +0500

    Task04 [Veresnikov]

--- task04/src/com/example/task04/Task04Main.java
+++ task04/src/com/example/task04/Task04Main.java
@@ -1,11 +1,23 @@
 package com.example.task04;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class Task04Main {
 
     public static void main(String[] args) {
-
-        // your implementation here
-
+        BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+        Pattern pattern = Pattern.compile("[^a-zа-яё0-9]");
+        Stream<String> stream = input.lines().flatMap(line -> pattern.splitAsStream(line.toLowerCase()));
+        Map<String, Long> map = stream.collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+        map.entrySet().stream().filter(s -> !s.getKey().equals(""))
+                .sorted(Map.Entry.<String, Long>comparingByValue().reversed()
+                        .thenComparing(Function.identity(), Map.Entry.comparingByKey()))
+                .limit(10).forEachOrdered(entry -> System.out.print(entry.getKey()+"\n"));
     }
-
 }
