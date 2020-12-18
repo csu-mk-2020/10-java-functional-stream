cb4467c69065f10dcf41e378fb9770bcf29d1f2b
Jester <zavyalov.mikhail99@gmail.com>
2020-12-18 15:51:05 +0500

    task02[Zavyalov]

--- task02/src/com/example/task02/Task02Main.java
+++ task02/src/com/example/task02/Task02Main.java
@@ -1,5 +1,6 @@
 package com.example.task02;
 
+import java.util.function.IntSupplier;
 import java.util.stream.IntStream;
 
 public class Task02Main {
@@ -15,9 +16,21 @@ public class Task02Main {
     }
 
     public static IntStream cycleGrayCode(int n) {
+        if (n < 1 || n > 16)
+            throw new IllegalArgumentException("n must be < 1 and > 16");
 
-        return null; // your implementation here
-
+        return IntStream.generate(new IntSupplier() {
+            int seed = 0;
+            final int period = (int) Math.pow(2, n);
+            final int mod = period - 1;
+            @Override
+            public int getAsInt() {
+                if (seed == period) seed = 0;
+                int num = (seed ^ (seed >> 1)) & mod;
+                ++seed;
+                return num;
+            }
+        });
     }
 
 }
