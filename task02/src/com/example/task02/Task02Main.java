ffcb10ca99e32440242c55946716e95070be2a62
lexa1329 <funin99@bk.ru>
2020-12-18 16:52:40 +0500

    Task02[Funin]

--- task02/src/com/example/task02/Task02Main.java
+++ task02/src/com/example/task02/Task02Main.java
@@ -1,6 +1,7 @@
 package com.example.task02;
 
 import java.util.stream.IntStream;
+import java.util.function.IntSupplier;
 
 public class Task02Main {
 
@@ -15,9 +16,26 @@ public class Task02Main {
     }
 
     public static IntStream cycleGrayCode(int n) {
+        if (n < 1 || n > 16) {
+            throw new IllegalArgumentException();
+        }
 
-        return null; // your implementation here
+        IntSupplier supplier = new IntSupplier() {
+            int seed = 0;
+            final int period = (int)Math.pow(2,n);
+            final int mod = period - 1;
+            @Override
+            public int getAsInt() {
+                if (seed == period) {
+                    seed = 0;
+                }
+                int num = (seed ^ (seed >> 1)) & mod;
+                ++seed;
+                return num;
+            }
+        };
 
+        return IntStream.generate(supplier);
     }
 
 }
