a207bc701ecbd610923a35c5552cb8c9995e52b5
Aleksey <alex-kadykov@bkru>
2020-12-24 14:33:36 +0500

    Task02 [Кадыков]

--- task02/src/com/example/task02/Task02Main.java
+++ task02/src/com/example/task02/Task02Main.java
@@ -1,5 +1,6 @@
 package com.example.task02;
 
+import java.util.function.IntSupplier;
 import java.util.stream.IntStream;
 
 public class Task02Main {
@@ -16,7 +17,26 @@ public class Task02Main {
 
     public static IntStream cycleGrayCode(int n) {
 
-        return null; // your implementation here
+        if (n < 1 || n > 16) {
+            throw new IllegalArgumentException();
+        }
+
+        IntSupplier supplier = new IntSupplier() {
+            int seed = 0;
+            final int period = (int)Math.pow(2,n);
+            final int mod = period - 1;
+            @Override
+            public int getAsInt() {
+                if (seed == period) {
+                    seed = 0;
+                }
+                int num = (seed ^ (seed >> 1)) & mod;
+                ++seed;
+                return num;
+            }
+        };
+
+        return IntStream.generate(supplier);
 
     }
 
